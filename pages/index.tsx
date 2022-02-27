6ca94b4730da286bf56e6d9b84e7957ead253e5f
davidadeola_ <davidolorunsesan15@gmail.com>
2022-02-27 23:55:55 +0100

    Main body commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,11 @@ export default function Home() {
       <main className='max-w-7xl mx-auto px-8 sm:px-16'>
         <section className='pt-6'>
           <h2 className='text-3xl font-semibold pb-5'>Inspiration for your next trip</h2>
-            <h1></h1>
+            <h1>London</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Id quisquam eius autem ullam, adipisci omnis modi tenetur
+              amet voluptates non totam doloremque sed, voluptate quae beatae 
+              debitis voluptatum? Temporibus, dolores.</p>
         </section>
       </main>
     </div>
