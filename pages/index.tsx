7c01c1938714b6e3d72091e9e7a2a4c47be22f23
davidadeola_ <davidolorunsesan15@gmail.com>
2022-02-28 23:15:28 +0100

    color commit
    FIne tunning

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <section className='pt-6'>
           <h2 className='text-3xl font-semibold pb-5'>Inspiration for your next trip</h2>
             <h1>London</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            <p className='text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
               Id quisquam eius autem ullam, adipisci omnis modi tenetur
               amet voluptates non totam doloremque sed, voluptate quae beatae 
               debitis voluptatum? Temporibus, dolores.</p>
